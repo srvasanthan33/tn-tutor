90a4e23f6d93cf5dde0ffa6bc41d09d2c4e140d9
Vasanthan <srvasanthan33@outlook.com>
2024-10-17 01:47:56 +0530

    Reusable Button Done

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Button from './components/Button'
+import ButtonSvg from "./assets/svg/ButtonSvg"
+import Header from './components/Header'
+
+function App() {
+
+	return (
+		<>
+
+
+			<div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+				<Header/>
+			</div>
+			<ButtonGradient />
+		</>
+	)
+}
+
+export default App
