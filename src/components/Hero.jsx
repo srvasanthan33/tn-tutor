d1d0c76da4a930bf907f65dfbf85536e536c6a4c
Vasanthan <srvasanthan33@outlook.com>
2024-10-21 10:29:31 +0530

    removed border

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,7 +47,7 @@ const Hero = () => {
         </div>
         
         <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
-          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+          <div className="relative z-1 p-0.5 rounded-2xl">
             <div className="relative bg-n-8 rounded-[1rem]">
               <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
 
